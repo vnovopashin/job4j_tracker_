c81fd8051e1c3ddbe2981467cae56aa36ba9b355
nova <novopashinva@gmail.com>
2026-02-25 22:32:16 +0700

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+    private String name;
+    private String surname;
+
+    Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
