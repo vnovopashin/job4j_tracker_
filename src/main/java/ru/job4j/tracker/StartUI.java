5b2db6a0fe24661268439c7a61c08ce8e5d1ea9c
nova <novopashinva@gmail.com>
2026-05-01 15:02:16 +0700

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import ru.job4j.tracker.action.*;
+import ru.job4j.tracker.input.ConsoleInput;
 import ru.job4j.tracker.input.Input;
 import ru.job4j.tracker.input.ValidateInput;
 import ru.job4j.tracker.output.ConsoleOutput;
@@ -36,7 +37,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ValidateInput();
+        Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output),
--- src/main/java/ru/job4j/tracker/input/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/input/ValidateInput.java
@@ -1,19 +1,46 @@
 package ru.job4j.tracker.input;
 
-public class ValidateInput extends ConsoleInput {
+import ru.job4j.tracker.output.Output;
+
+public class ValidateInput implements Input {
+    private final Output output;
+    private final Input input;
+
+    public ValidateInput(Output output, Input input) {
+        this.output = output;
+        this.input = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return input.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
-            try {
-                value = super.askInt(question);
-                invalid = false;
-            } catch (NumberFormatException nfe) {
+            String result = input.askStr(question);
+            if (!isNumber(result)) {
                 System.out.println("Пожалуйста, введите корректные данные");
+                continue;
             }
+            value = Integer.parseInt(result);
+            invalid = false;
         } while (invalid);
         return value;
+    }
+
+    private boolean isNumber(String value) {
+        boolean result = true;
+        char[] check = value.toCharArray();
+        for (char number : check) {
+            if (number < 48 || number > 57) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
